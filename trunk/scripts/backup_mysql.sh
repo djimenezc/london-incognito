79b510e4f3c4e34a8aa81aac0b79a82d2e374c66
david.jimenez19 <david.jimenez19@c61324e4-79ac-5a1c-4890-6254f9d9ece9>
2010-08-28 19:11:17 +0000

    modify script backup database of linux to use user name and password

--- trunk/scripts/backup_mysql.sh
+++ trunk/scripts/backup_mysql.sh
@@ -2,5 +2,7 @@
 
 export FILE=$2
 export BBDD=$1
+export password=$4
+export user=$3
 echo Backup of $BBDD in path $FILE
-mysqldump --add-drop-database -u root $BBDD > $FILE
+mysqldump --add-drop-database -u $user --password=$password $BBDD > $FILE
